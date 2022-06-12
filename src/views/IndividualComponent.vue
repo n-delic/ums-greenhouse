a469ed8cd22986616048fbe3be2f27628085f5d9
nakii99 <nahid.delic@fet.ba>
2022-06-12 15:55:41 +0200

    height fix 2

--- src/views/IndividualComponent.vue
+++ src/views/IndividualComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid justify-items-center gap-2 m-5 font-sans text-2xl font-bold grid-cols-4 ">
+    <div class="grid h-screen justify-items-center gap-2 m-5 font-sans text-2xl font-bold grid-cols-4 ">
         <h1 class="col-span-4 m-1">{{ currSensorInfo.name }}</h1>
         <CardComponent :info="currSensorInfo" :value="value" class="md:col-span-2 col-span-4"></CardComponent>
         <CardComponent :info="currSensorInfo" graph class="md:col-span-2 col-span-4"></CardComponent>

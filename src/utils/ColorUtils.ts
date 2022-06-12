c2293a457518a324408a20a421a6f66403632317
nakii99 <nahid.delic@fet.ba>
2022-06-12 13:49:28 +0200

    fixed threshold issue

--- src/utils/ColorUtils.ts
+++ src/utils/ColorUtils.ts
@@ -3,7 +3,7 @@ import Sensor from "@/types/Sensor";
 export function generateColor(sensorInfo: Sensor, sensorValue: number) : string {
     let oldKey = 0;
     for(const [key,value] of sensorInfo.threshold) {
-        if(oldKey < sensorValue && sensorValue < key) {
+        if(oldKey <= sensorValue && sensorValue <= key) {
             return value;
         }
         oldKey = key;
